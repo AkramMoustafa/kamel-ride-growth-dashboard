5d106150599d8c817a537c8c240dd4f046bbd6b7
AkramMoustafa <moust1am@cmich.edu>
2026-04-30 04:48:47 -0400

    Deployment changes

--- src/components/TrendCard.tsx
+++ src/components/TrendCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { getDailyTrend, getTrendInsight, getDailyReferralRate } from "../utils/analytics";
 import { colors, font, fontSize, radius, text } from "../styles/tokens";
 
